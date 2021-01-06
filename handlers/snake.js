b8ae243f7e7640e1282e5412baccaab8487c6afe
Keith Meyer <meyerkm6@miamioh.edu>
2021-01-05 23:50:16 -0500

    Moving to Acorn Interpreter

--- handlers/snake.js
+++ handlers/snake.js
@@ -1,9 +1,19 @@
+var img;
+
 var x = 50;
 var y = 50;
 var dir = 0;
 
+var realX = 50;
+var realY = 50;
+var realDir = 0;
+
 function init(){
     setInterval(render, 300);
+    img = new Image();
+    img.src = "https://spicyyoghurt.com/img/tutorials/develop_a_html5_game/sprite_animation.png";
+    img.decode();
+
 }
 
 function reset(){
@@ -15,18 +25,38 @@ function start(){
 }
 
 function tick(){
-    //render();
+
+    if(x == realX && y == realY){
+        return true;
+    }
+
+    if(realX != x)
+        realX += x > realX ? 1 : -1;
+    if(realY != y)
+        realY += y > realY ? 1 : -1;
+
+    return false;
 
 }
 
 function render(){
     //console.log("Render " + x + " " + y);
-    var c = document.getElementById("resultCanvas");
-    var ctx = c.getContext("2d");
+    let c = document.getElementById("resultCanvas");
+    let ctx = c.getContext("2d");
     ctx.clearRect(0, 0, c.width, c.height);
 
     ctx.beginPath();
-    ctx.rect(x, y, 200, 200);
+    ctx.rect(realX, realY, 200, 200);
+
+    let frameWidth = 50;
+    let frameHeight = 61;
+
+    // Rows and columns start from 0
+    let row = 1;
+    let column = 3;
+
+    ctx.drawImage(img, column*frameWidth, row*frameHeight, frameWidth, frameHeight, realX, realY, 200, 200);
+    //ctx.drawImage(img, realX, realY, 200, 200);
     ctx.stroke();
 }
 
@@ -38,13 +68,10 @@ function move(){
         if(dir % 90 == 0)
             y += dir == 90 ? -50 : 50;
     }
-
-    tick();
 }
 
 function turn(direction){
     console.log("Turn " + direction);
     dir += direction == "RIGHT" ? -90 : 90;
     dir %= 360;
-    tick();
 }
